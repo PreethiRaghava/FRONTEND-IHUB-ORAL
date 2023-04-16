da62efb1f956cbbb00088bb18f5e13a0e4cf8bed
vm <vm@healthcare-data.iiit.ac.in>
2023-04-16 09:35:27 +0000

    removed minio  connect from frontend

--- src/components/minio.js
+++ src/components/minio.js
@@ -1,12 +1,4 @@
 var Minio = require("minio");
-console.log("before minio");
-var minioClient = new Minio.Client({
-  endPoint: process.env.REACT_APP_MINIO_ACCESS_IP,
-  port: parseInt(process.env.REACT_APP_MINIO_ACCESS_PORT),
-  useSSL: false,
-  accessKey: process.env.REACT_APP_MINIO_ACCESS_KEY,
-  secretKey: process.env.REACT_APP_MINIO_ACCESS_SECRET
-});
-console.log("after minio");
-var minioBucket = process.env.REACT_APP_DEV === "true" ? process.env.REACT_APP_MINIO_BUCKET_DEV : process.env.REACT_APP_MINIO_BUCKET_PROD
-export {minioClient, minioBucket}
+var minioClient = "";
+var minioBucket = "";
+export { minioClient, minioBucket }
